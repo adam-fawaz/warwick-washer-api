22c21eaffa62a2a20993f89488ab95b3997483be
Adam Fawaz <60557714+adam-fawaz@users.noreply.github.com>
2024-06-16 13:31:06 +0000

    Code which outputs formatted json

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 import requests
+import json
 
+# All the other parameters in the header are not needed
 headers = {
-    'Accept': 'application/json, text/plain, */*',
-    'Sec-Fetch-Site': 'cross-site',
-    'Accept-Language': 'en-GB,en;q=0.9',
-    'Sec-Fetch-Mode': 'cors',
-    'Host': 'api.alliancelslabs.com',
-    'Origin': 'https://wa.sqinsights.com',
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4.1 Safari/605.1.15',
-    'Connection': 'keep-alive',
-    'Referer': 'https://wa.sqinsights.com/',
-    'Sec-Fetch-Dest': 'empty',
-    'alliancels-organization-id': '652210',
+    'alliancels-organization-id': '652210'
 }
 
 response = requests.get('https://api.alliancelslabs.com/washAlert/machines/7907', headers=headers)
+
+data = response.json()
+json_string = json.dumps(data, indent=4)
+print(json_string)
